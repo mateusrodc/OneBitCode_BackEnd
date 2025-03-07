162db5af1830ad9248210fc12a98ead86c5c334d
Mateus Costa <mcmateus070@gmail.com>
2025-03-07 18:33:31 -0300

    configurando upload videos Admin

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,22 @@
 import { ResourceWithOptions } from "adminjs";
-import { Category } from "../../models";
+import { Category, Course, Episode  } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { courseResourceOptions } from "./course";
+import {  episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
+    resource: Course,
+    options: courseResourceOptions
+  },
+  {
     resource: Category,
     options: categoryResourceOptions
-  }
+  },
+  {
+    resource: Episode,
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
+  },
+
 ]
