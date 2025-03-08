739e2279aca8256cda48e35861a45dfe2e679e5f
Mateus Costa <mcmateus070@gmail.com>
2025-03-08 14:04:04 -0300

    obtendo cursos favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,9 @@ import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesControllers'
 import { authController } from './controllers/authController'
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { favoritesController } from './controllers/favoriteController'
 
 const router = express.Router()
-
-
 
 router.get('/categories', ensureAuth, categoriesController.index)
 router.get('/categories/:id', ensureAuth, categoriesController.show)
@@ -18,6 +17,9 @@ router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
+router.get('/favorites', ensureAuth, favoritesController.index)
+router.post('/favorites', ensureAuth, favoritesController.save)
 
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
