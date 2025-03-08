ab2b14c7d66b3f667a73c7db7723fd5d77199f6b
Mateus Costa <mcmateus070@gmail.com>
2025-03-08 09:01:54 -0300

    api stream video

---
+++ src/controllers/coursesController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express'
+import { courseService } from '../services/courseService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
+
+export const coursesController = {
+
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const course = await courseService.findByIdWithEpisodes(id)
+      return res.json(course)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  featured: async (req: Request, res: Response) => {
+    try {
+      const featuredCourses = await courseService.getRandomFeaturedCourses()
+      return res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  newest: async (req: Request, res: Response) => {
+    try {
+      const newestCourses = await courseService.getTopTenNewest()
+      return res.json(newestCourses)
+    } catch (err) {
+			if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  search: async (req: Request, res: Response) => {
+
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
+
+    try{
+
+      if (typeof name !== 'string') throw new Error('name param must be of type string');
+
+      const courses = await courseService.findByName(name, page, perPage)
+
+      return res.json(courses)
+
+    } catch(err){
+
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+
+    }
+
+  }
+}
---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { categoriesController } from './controllers/categoriesControllers'
+import { coursesController } from './controllers/coursesController'
+import { episodesController } from './controllers/episodesControllers'
+
+const router = express.Router()
+
+router.get('/categories', categoriesController.index)
+router.get('/categories/:id', categoriesController.show)
+
+router.get('/courses/featured', coursesController.featured)
+router.get('/courses/newest', coursesController.newest)
+router.get('/courses/search', coursesController.search)
+router.get('/courses/:id', coursesController.show)
+
+router.get('/episodes/stream', episodesController.stream)
+
+
+
+export { router }
