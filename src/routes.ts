27d0d146eeae9e2fdd1bd6e808fa9d55ccaead04
Mateus Costa <mcmateus070@gmail.com>
2025-03-08 09:37:04 -0300

    registro de usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { categoriesController } from './controllers/categoriesControllers'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesControllers'
+import { authController } from './controllers/authController'
 
 const router = express.Router()
 
@@ -14,6 +15,8 @@ router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
 router.get('/episodes/stream', episodesController.stream)
+
+router.post('/auth/register', authController.register)
 
 
 
