8e1f5437b58fbcab29b9a6a5ffd24365676f0210
Mateus Costa <mcmateus070@gmail.com>
2025-03-07 23:15:10 -0300

    personalizando Dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,10 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { database } from '../database'
 import { adminJsResources } from './resources'
+import { Category, Course, Episode, User } from '../models'
+import bcrypt from 'bcrypt'
+import { locale } from './locale'
+
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
@@ -10,6 +14,23 @@ export const adminJs = new AdminJs({
   databases: [database],
   rootPath: '/admin',
   resources: adminJsResources,
+  dashboard: {
+    component: AdminJs.bundle('./components/Dashboard'),
+    handler: async (req, res, context) => {
+      const courses = await Course.count()
+      const episodes = await Episode.count()
+      const category = await Category.count()
+      const standardUsers = await User.count({ where: { role: 'user' } })
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': category,
+        'Usuários': standardUsers
+      })
+    },
+  },
+  locale: locale,
   branding: {
     companyName: 'OneBitFlix',
     logo: '/onebitflix.svg',
@@ -33,4 +54,22 @@ export const adminJs = new AdminJs({
   }
 })
 
-export const adminJsRouter = AdminJsExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(adminJs, {
+  authenticate: async (email, password) => {
+    const user = await User.findOne({ where: { email } })
+
+    if (user && user.role === 'admin') {
+      const matched = await bcrypt.compare(password, user.password)
+
+      if (matched) {
+        return user
+      }
+    }
+
+    return false
+  },
+  cookiePassword: 'senha-do-cookie'
+}, null, {
+	resave: false,
+	saveUninitialized: false
+})
