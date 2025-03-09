e2e2174a1abea7cd62c0278f83843efcdbb71fc5
Mateus Costa <mcmateus070@gmail.com>
2025-03-09 11:23:58 -0300

    update user

--- src/routes.ts
+++ src/routes.ts
@@ -33,6 +33,9 @@ router.post('/auth/login', authController.login)
 router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
 
+router.get('/users/current', ensureAuth, usersController.show)
 router.get('/users/current/watching', ensureAuth, usersController.watching)
+router.put('/users/current', ensureAuth, usersController.update)
+router.put('/users/current/password', ensureAuth, usersController.updatePassword)
 
 export { router }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -87,5 +87,31 @@ export const userService = {
       keepWatchingList.sort((a, b) => a.watchTime.updatedAt < b.watchTime.updatedAt ? 1 : -1)
       return keepWatchingList
 
-  }
+  },
+
+	update: async (id: number, attributes: {
+    firstName: string
+    lastName: string
+    phone: string
+    birth: Date
+    email: string
+  }) =>
+  {
+
+      const [affectedRows, updatedUsers] = await User.update(attributes, { where: { id }, returning: true })
+
+      return updatedUsers[0]
+  },
+
+  updatePassword: async (id: string | number, password: string) => {
+    const [affectedRows, updatedUsers] = await User.update({
+      password
+    }, {
+      where: { id },
+      individualHooks: true,
+      returning: true
+    })
+
+    return updatedUsers[0]
+  },
 }
