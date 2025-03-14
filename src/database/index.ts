bcb161c059d3c8d1d54a0cdfdb4b10490cef3f65
Mateus Costa <mcmateus070@gmail.com>
2025-03-14 12:59:33 -0300

    ajustes ssl

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,5 +4,12 @@ import { DATABASE_URL } from '../config/enviroment'
 export const database = new Sequelize(DATABASE_URL,{
 	define: {
     underscored: true
+  },
+  dialect: 'postgres',
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 })
